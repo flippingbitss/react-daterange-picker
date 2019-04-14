2e1da2796de34ad8f58d64f60548b3851e251395
Manpreet Singh Matharu <matharumanpreet00@gmail.com>
2019-04-14 14:14:14 -0400

    first gh-pages published

---
+++ precache-manifest.14151c32f2d9371e07b48535737253a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a2bec0bcc554bddba8",
+    "url": "/static/css/main.078dc41c.chunk.css"
+  },
+  {
+    "revision": "83a2bec0bcc554bddba8",
+    "url": "/static/js/main.325fc285.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99e596fcf8fc01304d5d",
+    "url": "/static/js/2.86843f13.chunk.js"
+  },
+  {
+    "revision": "cc7358783e8e1241920b8a7673caed7a",
+    "url": "/index.html"
+  }
+];
